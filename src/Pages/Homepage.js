1a8b02fea9140768aa03ecb0a96b3e58a1f63dc2
Khoa Dang <khoa9205141@gmail.com>
2022-11-23 16:31:47 +0700

    homepage

---
+++ src/Pages/Homepage.js
@@ -0,0 +1,58 @@
+import {
+  Box,
+  Center,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Homepage() {
+  const history = useNavigate();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (user) history.push("/chats");
+  }, [history]);
+
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Center>
+          <Text fontSize="4xl" fontFamily="Work sans" color="blue.500">
+            Zafa
+          </Text>
+        </Center>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>{/* <Login /> */}</TabPanel>
+            <TabPanel>{/* <Signup /> */}</TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+}
+
+export default Homepage;
