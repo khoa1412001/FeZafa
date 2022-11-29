913f4e30bdea590c3e335df7814b6806ee369c8d
Khoa Dang <khoa9205141@gmail.com>
2022-11-30 00:18:59 +0700

    stuff

--- src/Context/ChatProvider.js
+++ src/Context/ChatProvider.js
@@ -1,5 +1,26 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
 const ChatContext = createContext();
 
-const ChatProvider = ({ children }) => {};
+const ChatProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  // const navigate = useNavigate();
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    setUser(userInfo);
+    // if (!userInfo) navigate("/");
+  }, []); //navigate]);
+
+  return (
+    <ChatContext.Provider value={{ user, setUser }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const ChatState = () => {
+  return useContext(ChatContext);
+};
+export default ChatProvider;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import ChatProvider from "./Context/ChatProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </BrowserRouter>
+  <ChakraProvider>
+    <BrowserRouter>
+      <ChatProvider>
+        <App />
+      </ChatProvider>
+    </BrowserRouter>
+  </ChakraProvider>
 );
